35f9c548710491173f0f88f3b1404b6df7175138
Grasiela Gomes <produtoradama@gmail.com>
2021-08-24 20:47:56 -0300

    inserting miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Site',
+          amount: 3000,
+          type: 'deposit',
+          category: 'Design',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
