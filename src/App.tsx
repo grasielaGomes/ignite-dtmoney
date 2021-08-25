975978900a0d75f5f28419f604c8522c82267b31
Grasiela Gomes <produtoradama@gmail.com>
2021-08-25 14:18:54 -0300

    styling modal form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard/dashboard";
 import { Header } from "./components/Header/header";
+import { NewTransactionModal } from './components/NewTransactionModal/new-transaction-modal';
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal () {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal () {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal}/>
       <GlobalStyle />
     </>
   );
---
+++ src/components/NewTransactionModal/new-transaction-modal-styles.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Container = styled.form`
+  h2 {
+    color: var(--text-title);
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+
+  input {
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 3.5rem;
+    border-radius: 0.25rem;
+    border: 1px solid #d7d7d7;
+    background-color: #e7e9ee;
+    font-size: 1rem;
+
+    &::placeholder {
+      color: var(--text-body);
+    }
+
+    & + input {
+      margin-top: 1rem;
+    }
+  }
+
+  button[type="submit"] {
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 3.5rem;
+    background-color: var(--green);
+    color: var(--background);
+    border: none;
+    border-radius: 0.25rem;
+    font-size: 1rem;
+    margin-top: 1.5rem;
+    transition: filter 300ms;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`;
